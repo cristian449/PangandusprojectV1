456032ed33ad37286aaf71fa61f1c9dabb6274fe
cristian449 <144561600+cristian449@users.noreply.github.com>
2025-06-04 13:25:06 +0300

    Added LoginViewModel added RegisterViewModel as well as Model for User also added another package

---
+++ PangandusProjectv1/PangandusProjectv1/Models/LoginViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PangandusProjectv1.Models
+{
+    public class LoginViewModel
+    {
+            [Required]
+            [EmailAddress]
+            public string Email { get; set; }
+
+            [Required]
+            [DataType(DataType.Password)]
+            public string Password { get; set; }
+
+            public bool RememberMe { get; set; } //Not Sure if this will work, just more so added this in for style more than anything else
+        }
+
+    }
+
